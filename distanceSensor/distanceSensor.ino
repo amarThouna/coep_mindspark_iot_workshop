9715c70047e6b410888cf20cdb92f63981147806
AMARJIT THOUNA <amarsingh@ptc.com>
2017-08-22 17:33:16 +0530

    adding comments and made more customizable

--- distanceSensor/distanceSensor.ino
+++ distanceSensor/distanceSensor.ino
@@ -1,34 +1,39 @@
 #include <SPI.h>
 #include <Ethernet.h>
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; 
-char server[] = "server";
 EthernetClient client;
 
-char appKey[] = "app-key";
-char thingName[] = "ThingMaster";
-char serviceName[] = "setHeartBeat";
-int timeBetweenRefresh = 1000;
+char server[] = "server"; //Thingworx server hostname. Change this to your server
+int port=8080; //Thingworx server port.
+char appKey[] = "<your_app-key>"; //App key you created
+char thingName[] = "<DistenceSensor_<username>>"; //Thing Name you created for getting sensor value
+char serviceName[] = "setDistance"; //service name
+char propertyName[]="distance";
+
+int timeBetweenRefresh = 1000; //refresh interval in milliseconds
 unsigned long lastConnectionTime = 0;
 
-const int trigPin = 2;
+const int trigPin = 2; 
 const int echoPin = 3;
 float duration, distance;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Trying to get an IP address using DHCP");
-  Ethernet.begin(mac);
-  Serial.print("My IP address: ");  Serial.print(Ethernet.localIP());  Serial.println();
+  Ethernet.begin(mac);      //Initialize the ethernet with the provided MAC. Ethernet sheild will also get an IP thought DHCP
+  Serial.print("My IP address: ");
+  Serial.print(Ethernet.localIP());
+  Serial.println();
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
 void loop() {
   if (millis() - lastConnectionTime > timeBetweenRefresh){
-    updateValues();    
+    updateValues();   //uploading data to Thingworx
   }    
   else{
-    updateSensorsValue();
+    updateSensorsValue(); //updating senror data locally
   }
 }
 
@@ -48,10 +53,10 @@ void updateSensorsValue(){
 
 void updateValues()
 {
-  if (client.connect(server, 8080)) {
+  if (client.connect(server, port)) {  
       if(client.connected()){
         // Sending a header of a network packet  
-        Serial.println("Sending data to IoT server...\n");  
+        Serial.println("Sending data to Thingworx Server...\n");  
         Serial.print("POST /Thingworx/Things/");  client.print("POST /Thingworx/Things/"); 
         Serial.print(thingName);  client.print(thingName);  
         Serial.print("/Services/");  client.print("/Services/");  
@@ -60,7 +65,7 @@ void updateValues()
         Serial.print(appKey);        client.print(appKey);  
         Serial.print("&method=post&x-thingworx-session=true");  client.print("&method=post&x-thingworx-session=true"); 
         Serial.print("&");  client.print("&");  
-        Serial.print("hb"); client.print("hb");  
+        Serial.print(propertyName); client.print(propertyName);  
         Serial.print("=");  client.print("=");
       
         Serial.print(distance);  client.print(distance);  
