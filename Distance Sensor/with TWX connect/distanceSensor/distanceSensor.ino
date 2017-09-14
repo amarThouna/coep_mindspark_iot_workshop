d3542d1232faf931ed413f5ba9286e6c97441e4c
AMARJIT THOUNA <amarsingh@ptc.com>
2017-09-14 14:19:57 +0530

    Changes for MindSpark 2017

--- Distance Sensor/with TWX connect/distanceSensor/distanceSensor.ino
+++ Distance Sensor/with TWX connect/distanceSensor/distanceSensor.ino
@@ -1,45 +1,48 @@
 #include <SPI.h>
 #include <Ethernet.h>
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; 
+
+const int trigPin = 3; 
+const int echoPin = 2; 
+ 
+int duration;
+float distance;
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; //Make sure your mac address is unique in your network
 EthernetClient client;
 
-char server[] = "server"; //Thingworx server hostname. Change this to your server
-int port=8080; //Thingworx server port.
+char server[] = "server"; //ThingWorx server hostname. Change this to your server
+int port=8080; //ThingWorx server port. Change as per your server port
 char appKey[] = "<your_app-key>"; //App key you created
-char thingName[] = "<DistenceSensor_<username>>"; //Thing Name you created for getting sensor value
-char serviceName[] = "setDistance"; //service name
+char thingName[] = "<DistenceSensor_<username>>"; //Thing Name Should match your thing in ThingWorx
+char serviceName[] = "setDistance"; //service name should be same exist in the Thing Name you specified
 char propertyName[]="distance";
 
 int timeBetweenRefresh = 1000; //refresh interval in milliseconds
 unsigned long lastConnectionTime = 0;
 
-const int echoPin = 2; 
-const int trigPin = 3;  
-int duration;
-float distance;
-
 void setup() {
   Serial.begin(9600);
-  //initializing ethernet
+  
+  Serial.print("initializing ethernet");
   Serial.println("Trying to get an IP address using DHCP");
   Ethernet.begin(mac);      //Initialize the ethernet with the provided MAC. Ethernet sheild will also get an IP thought DHCP
   Serial.print("My IP address: ");
   Serial.print(Ethernet.localIP());
   Serial.println();
-  //initializing arduino pins
+  
+  Serial.print("initializing arduino pins");
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
 void loop() {
   if (millis() - lastConnectionTime > timeBetweenRefresh){
-    updateValues();   //uploading data to Thingworx
+    updateValues();   //uploading data to ThingWorx
   }    
   else{
     updateSensorsValue(); //updating senror data locally
   }
 }
-
 void updateSensorsValue(){
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
@@ -59,7 +62,7 @@ void updateValues()
   if (client.connect(server, port)) {  
       if(client.connected()){
         // Sending a header of a network packet  
-        Serial.println("Sending data to Thingworx Server...\n");  
+        Serial.println("Sending data to ThingWorx Server...\n");  
         Serial.print("POST /Thingworx/Things/");  client.print("POST /Thingworx/Things/"); 
         Serial.print(thingName);  client.print(thingName);  
         Serial.print("/Services/");  client.print("/Services/");  
@@ -84,8 +87,8 @@ void updateValues()
         lastConnectionTime = millis();
       }
     }
-    else{
-      Serial.println("Connection could not be established");  
-      client.stop();
-    }
-  }
+  else{
+    Serial.println("Connection could not be established");  
+    client.stop();
+   }
+}
